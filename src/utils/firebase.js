6d7d2b45e1a09641420e1def450681689e7cabdd
Tomi <garciadelriotomas@gmail.com>
2023-10-31 13:29:38 -0300

    test

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,13 +4,13 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 import {getDatabase,set,get,ref,update,off,push,onValue} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket:import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId:import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId:import.meta.env.VITE_APP_ID,
-  databaseURL:import.meta.env.VITE_DATABASE_URL,
+  apiKey: "AIzaSyA1zmiWZ3CrEKwOv1_mnprsXUDiHRuF0Wo",
+  authDomain: "yo-invierto-development.firebaseapp.com",
+  databaseURL: "https://yo-invierto-development-default-rtdb.firebaseio.com",
+  projectId: "yo-invierto-development",
+  storageBucket: "yo-invierto-development.appspot.com",
+  messagingSenderId: "735172767787",
+  appId: "1:735172767787:web:aa7d8446ba74ac313a7a6e"
 };
 
 // Initialize Firebase
